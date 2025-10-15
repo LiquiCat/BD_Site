6de80a9219fd9e1afa833f8599b87b9745200e36
LiquiCat <arseniy.priymakov@gmail.com>
2025-10-16 00:10:50 +0400

    generating publick folder from static

--- src/main.py
+++ src/main.py
@@ -1,8 +1,38 @@
-from textnode import TextNode, TextType
+import os
+from shutil import rmtree, copy
+
+def copy_process():
+    working = os.getcwd()
+    public_dir = os.path.join(working, "public")
+    static_dir = os.path.join(working, "static")
+
+    if os.path.exists(public_dir):
+        rmtree(public_dir)
+    os.mkdir(public_dir)
+
+    copy_to_public(static_dir, public_dir, "")
+
+def copy_to_public(static_dir, public_dir, folder_relative):
+    cur_dir_s = os.path.join(static_dir, folder_relative)
+    cur_dir_p = os.path.join(public_dir, folder_relative)
+
+    print(f"{static_dir=}")
+    print(f"{folder_relative=}")
+    print(f"{cur_dir_s=}")
+    
+
+    for item in os.listdir(cur_dir_s):
+        path_in_static = os.path.join(cur_dir_s, item)
+        if os.path.isfile(path_in_static):
+            copy(path_in_static, cur_dir_p)
+        else:
+            path_in_public = os.path.join(cur_dir_p, item)
+            os.mkdir(path_in_public)
+            extended_path = os.path.join(folder_relative, item)
+            copy_to_public(static_dir, public_dir, extended_path)
 
 def main():
-    t_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(t_node)
+    copy_process()
 
 if __name__ == "__main__":
     main()
